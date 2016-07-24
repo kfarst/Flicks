06ff7358cefdd3a1351f175311d1c6a185a22d11
Kevin Farst <kevin.farst@zozi.com>
2016-07-24 14:53:14 -0700

    Custom refresh view, add API error reporting

--- app/src/main/java/com/example/kfarst/flicks/activities/MovieListActivity.java
+++ app/src/main/java/com/example/kfarst/flicks/activities/MovieListActivity.java
@@ -2,19 +2,21 @@ package com.example.kfarst.flicks.activities;
 
 import android.app.Activity;
 import android.content.res.Configuration;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.kfarst.flicks.models.Movie;
 import com.example.kfarst.flicks.adapters.MovieItemsAdapter;
 import com.example.kfarst.flicks.api.MoviesApiClient;
 import com.example.kfarst.flicks.R;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.tuesda.walker.circlerefresh.CircleRefreshLayout;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,7 +27,7 @@ import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 
 public class MovieListActivity extends AppCompatActivity {
-    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
+    @BindView(R.id.refresh_layout) CircleRefreshLayout swipeContainer;
     @BindView(R.id.lvMovies) RecyclerView lvMovies;
 
     private ArrayList<Movie> movies = new ArrayList<Movie>();
@@ -40,20 +42,16 @@ public class MovieListActivity extends AppCompatActivity {
         lvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         // Setup refresh listener which triggers new data loading
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swipeContainer.setOnRefreshListener(new CircleRefreshLayout.OnCircleRefreshListener() {
             @Override
-            public void onRefresh() {
-                // Your code to refresh the list here.
-                // Make sure you call swipeContainer.setRefreshing(false)
-                // once the network request has completed successfully.
+            public void completeRefresh() {
+            }
+
+            @Override
+            public void refreshing() {
                 fetchMovies();
             }
         });
-        // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
 
         fetchMovies();
     }
@@ -75,7 +73,6 @@ public class MovieListActivity extends AppCompatActivity {
                     if (movies.size() > 0) {
                         moviesAdapter.clear();
                         moviesAdapter.addAll(Movie.fromJSON(movieList.getJSONArray("results")));
-                        swipeContainer.setRefreshing(false);
                     } else {
                         movies = Movie.fromJSON(movieList.getJSONArray("results"));
                         moviesAdapter = new MovieItemsAdapter(listActivity, movies);
@@ -88,9 +85,21 @@ public class MovieListActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                Log.e("ERROR", t.toString());
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("ERROR", throwable.toString());
+                String errorString = getString(R.string.error_fetching_movies);
+
+                if (movies.size() > 0) {
+                    Toast.makeText(getBaseContext(), errorString + ' ' + getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(), errorString + ' ' + getString(R.string.please_restart_app), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                swipeContainer.finishRefreshing();
             }
         });
     }
